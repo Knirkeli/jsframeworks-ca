5d7baaa0e89de697d739bef6df944f109608a500
Eirik Kirkeli <knirkeli88@msn.com>
2024-04-07 23:20:39 +0200

    Refactor product page component and update useEffect dependencies

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -1,78 +1,3 @@
-// import React from "react";
-// import { useState } from "react";
-// import { Product } from "../../app/components/Products";
-// import Image from "next/image";
-// import Link from "next/link";
-// import Navbar from "../../components/Navbar";
-// import Footer from "../../components/Footer";
-// import "../../app/globals.css";
-
-// interface ProductPageProps {
-//   product: Product;
-// }
-
-// const ProductPage: React.FC<ProductPageProps> = ({ product }) => {
-//   const [productData, setProductData] = useState(product);
-
-//   console.log("productData:", productData);
-
-//   if (!productData || !productData.data) {
-//     return (
-//       <div>
-//         <Navbar />
-//         <div>
-//           No product data available. Please check the product ID or your fetch
-//           URL.
-//         </div>
-//         <Footer />
-//       </div>
-//     );
-//   }
-
-//   const { data } = productData;
-
-//   return (
-//     <div>
-//       <Navbar />
-//       <div>
-//         <h1>{data.title}</h1>
-//         {data.image && <img src={data.image.url} alt={data.image.alt} />}
-//         <p>Price: {data.price}</p>
-//         <p>Rating: {data.rating}</p>
-//         <h2>Reviews:</h2>
-//         {data.reviews &&
-//           data.reviews.map((review, index) => (
-//             <div key={index}>
-//               <h4>{review.username}</h4>
-//               <p>{review.description}</p>
-//             </div>
-//           ))}
-//       </div>
-//       <Footer />
-//     </div>
-//   );
-// };
-
-// export async function getServerSideProps(context) {
-//   const { id } = context.params;
-//   let product = null;
-
-//   try {
-//     const res = await fetch(`https://v2.api.noroff.dev/online-shop/${id}`);
-//     if (res.ok) {
-//       product = await res.json();
-//     } else {
-//       console.error("Failed to fetch product:", res.status, res.statusText);
-//     }
-//   } catch (error) {
-//     console.error("Failed to fetch product:", error);
-//   }
-
-//   return {
-//     props: { product }, // will be passed to the page component as props
-//   };
-// }
-
 // export default ProductPage;
 import React, { useState, useEffect } from "react";
 import { Product } from "../../app/components/Products";
@@ -99,7 +24,7 @@ const ProductPage: React.FC<ProductPageProps> = ({ product }) => {
 
   useEffect(() => {
     setTotalItems(getTotalNumberOfItemsInCart());
-  }, []); // Run this function when the component mounts
+  }, [getTotalNumberOfItemsInCart]); // Run this function when the component mounts
 
   useEffect(() => {
     const unsubscribe = useProductStore.subscribe(() => {
