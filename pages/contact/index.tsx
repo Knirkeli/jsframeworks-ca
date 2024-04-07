e9852b1b397ee2ea9ea0e49caf72d24d16675eb0
Eirik Kirkeli <knirkeli88@msn.com>
2024-04-08 00:08:09 +0200

    Refactor handleChange function to handle both input and textarea elements

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -11,7 +11,9 @@ const ContactPage: React.FC = () => {
     body: "",
   });
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
@@ -69,7 +71,9 @@ const ContactPage: React.FC = () => {
             <textarea
               name="body"
               value={formData.body}
-              onChange={handleChange}
+              onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
+                handleChange(e)
+              }
               required
               minLength={3}
               className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 border border-gray-200"
